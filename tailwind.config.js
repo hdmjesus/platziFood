10ebbcc4a1fb0db0dc51df0bd9654cf96459c209
Jesus Hernandez <hdmjesus@gmail.com>
2020-08-13 15:25:25 -0400

    End

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+        primary: '#f3c614',
+        secondary: '#353535',
+      },
+      height: {
+        '80': '20rem',
+      },
+      boxShadow: {
+        xxg:
+          '0 10px 15px -3px rgba(0, 0, 0,.3), 0 4px 6px -2px rgba(0, 0, 0,.3)',
+      },
+    },
   },
-  variants: {},
+  variants: {
+    opacity: ['responsive', 'hover', 'focus', 'disabled'],
+  },
   plugins: [],
-}
+};
